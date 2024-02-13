6a3173e35c537e379194f7e6576f4d80f306cdea
Mamath <mathmath350@gmail.com>
2024-02-13 18:33:53 +0100

    execution on start

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@ CRON=${CRON:-0 0,6,12,18 * * *}
 echo "speedtest2mqtt has been started "
 
 declare | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /var/tmp/container.env
-sed -i "/schedule/c\    schedule: \"${CRON}\"" /home/foo/crontab.yml
+sed -i "/schedule1/c\    schedule: \"${CRON}\"" /home/foo/crontab.yml
+sed -i "/schedule2/c\    schedule: \"@reboot\"" /home/foo/crontab.yml
 
 echo "starting cron (${CRON})"
 /yacronenv/bin/yacron -c /home/foo/crontab.yml
